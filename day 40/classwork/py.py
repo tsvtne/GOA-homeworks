3f50607279318e312df546ff5fe7b50f461dd52e
tsvtne <cotnegulbani4@gmail.com>
2025-01-21 22:27:57 +0400

    second commit

---
+++ day 40/classwork/py.py
@@ -0,0 +1,26 @@
+# def friend(x):
+#     list = []
+#     for i in x:
+#         if len(i) == 4:
+#             list.append(i)
+#         return list
+    
+#     def friend(x):
+#     result = []
+#     for i in x:
+#         if len(i) == 4:
+#             result.append(i)
+#     return result
+
+
+# def validate_pin(pin):
+#     if pin.isdigit() and (len(pin) == 4 or len (pin) == 6):
+#         return True
+#     return false
+
+# def validate_pin(pin):
+#     if pin.isdigit() and (len(pin) == 4 or len(pin) == 6):
+#         return True
+#     return False
+
+        
